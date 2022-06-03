91776fcc5645ab25a9409f6a43714967a8ad7fdd
kiarie404 <kiarie404@gmail.com>
2022-06-03 07:35:14 -0700

    perfect backgroung video

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,10 +1,18 @@
 
-// ************* background_video actions ********************** //
+
+// ***************** background_video actions  ******************** //
 // initializing variables necessary for background_video display effects
 var background_video_src = "../media/videos/sky.mp4";
 var background_video = document.getElementById('background_video');
 
-// inserting video...
+// inserting video... at the beggining
 background_video.src = background_video_src;
-// making the video to always be in a loop
-if (background_video.paused){ background_video.play();  }
+
+//background_video should always be in a loop
+//background_video should always be on autoplay
+// looping and autoplay should be enforced when a page is loaded :
+window.onload = function() {
+    background_video.autoplay = true;
+    background_video.loop = true;
+    background_video.muted = true;
+}
