bef28bd34f85516f7b0c5c29154942297d430730
kiarie404 <kiarie404@gmail.com>
2022-06-03 07:10:11 -0700

    everlooping background ideo

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,7 +1,10 @@
 
+// ************* background_video actions ********************** //
 // initializing variables necessary for background_video display effects
 var background_video_src = "../media/videos/sky.mp4";
 var background_video = document.getElementById('background_video');
 
-// insert image
-background_image.src = background_image_src;
+// inserting video...
+background_video.src = background_video_src;
+// making the video to always be in a loop
+if (background_video.paused){ background_video.play();  }
